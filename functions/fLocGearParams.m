c610b5948f1000c79a690734f56ebe5c8cbf81cc
astigliani <astiglia@stanford.edu>
2018-08-14 13:12:33 -0700

    Added analysis code for Flywheel gear.

--- functions/fLocGearParams.m
+++ functions/fLocGearParams.m
@@ -23,32 +23,32 @@ init_params = mrInitDefaultParams;
 init_params.inplane      % char array, path to inplane file found by gear code (.nii.gz)
 init_params.functionals  % cell array, paths to fMRI data with filenames (.nii.gz)
 init_params.parfile      % cell array, paths to stimulus parameter files with filenames (.par)
-init_params.clip = clip; % int, number of TRs to clip from beginning of each run (initialize as 0)
 % for clipping TRs from countdown, default should be [2] for MUX and [countdown/TR] for non-MUX
+init_params.clip = clip; % number of TRs to clip from beginning of each run
 init_params.scanGroups = {1:num_runs}; % cell array of scan numbers to group
 
 % necessary fields for motion compensation
-init_params.motionCompRefFrame     = 8; % frame number of reference TR for within-scan compensation
-init_params.motionCompSmoothFrames = 3; % time window (in TRs) for within-scan compensation
-init_params.motionCompRefScan      = 1; % run number of reference scan for between-scans compensation
+init_params.motionCompRefFrame     = 8; % int, frame number of reference TR for within-scan compensation
+init_params.motionCompSmoothFrames = 3; % int, time window (in TRs) for within-scan compensation
+init_params.motionCompRefScan      = 1; % int, run number of reference scan for between-scans compensation
 
 % necessary fields that cannot be modified (maybe these can be hidden)
-init_params.sessionCode = session; % set to Flywheel exam number
-init_params.doAnalParams      = 1; % logical, set GLM analysis parameters during intialization
-init_params.doSkipFrames      = 1; % logical, clip countdown frames during initialization
-init_params.doPreprocessing   = 1; % logcial, do some preprocessing during initialization
-init_params.applyGlm          = 0; % logical, apply GLM during initialization
-init_params.applyCorAnal      = 0; % logical, unnecessary for GLM analysis
-init_params.motionComp        = 0; % logical, wait to do this until later
-% set init_params.keeFrames using the value of the "clip" field set above
+init_params.sessionCode     = session; % set to Flywheel exam number
+init_params.doAnalParams          = 1; % logical, set GLM analysis parameters during intialization
+init_params.doSkipFrames          = 1; % logical, clip countdown frames during initialization
+init_params.doPreprocessing       = 1; % logcial, do some preprocessing during initialization
+init_params.applyGlm              = 0; % logical, apply GLM during initialization
+init_params.applyCorAnal          = 0; % logical, unnecessary for GLM analysis
+init_params.motionComp            = 0; % logical, wait to do this until later
+init_params.sliceTimingCorrection = 0; % logical, wait to do this until later
+% set init_params.keeFrames using the "clip" field set above
 init_params.keepFrames = repmat([init_params.clip -1], num_runs, 1);
-init_params.sliceTimingCorrection = 0; % logical, no slice time correction durin initialization
 
 % unnecessary fields that can be filled in by user
-init_params.subject     = ''; % char array with participant ID
-init_params.description = ''; % char array describing session
-init_params.comments    = ''; % char array of comments
-init_params.annotations = {}; % cell array of descriptions for each run
+init_params.subject     = ''; % char array, participant ID
+init_params.description = ''; % char array, describes session
+init_params.comments    = ''; % char array, general comments about session
+init_params.annotations = {}; % cell array, description for each run
 
 
 %% Initialize and modify GLM analysis parameters
@@ -64,11 +64,11 @@ glm_params.glmHRF        = 3;  % set the HRF: 0 = deconvolve, 1 = estimate HRF f
 glm_params.glmWhiten     = 0;  % logical, controls whitening of data for GLM analysis
 
 % necessary fields that cannot be modified by user
-glm_params.eventAnalysis = 1;  % logical, run a standard GLM
-glm_params.lowPassFilter = 0;  % logical, do low-pass filtering
-glm_params.framePeriod         % TR of fMRI data in seconds
-glm_params.glmHRF_params       % generated with HRF depending on glm_params.glmHRF
-glm_params.eventsPerBlock      % int, number of TRs per block; can be calculated from parfile and TR
+glm_params.eventAnalysis = 1; % logical, run a standard GLM
+glm_params.lowPassFilter = 0; % logical, do low-pass filtering
+glm_params.framePeriod        % TR of fMRI data in seconds
+glm_params.glmHRF_params      % generated with HRF depending on glm_params.glmHRF
+glm_params.eventsPerBlock     % int, number of TRs per block; can be calculated from parfile and TR
 
 % visulization parameters that do not affect GLM fits
 glm_params.ampType    = 'betas'; % type of response amplitudes to visualize: 'difference' (raw), 'betas' (GLM betas), or 'deconvolved'

cef3807dbe0923cca1a6e5c375e79630c9fc1da6
Michael Perry <lmperry@stanford.edu>
2018-10-10 17:04:58 -0700

    Parse config file for needed params

--- functions/fLocGearRun.m
+++ functions/fLocGearRun.m
@@ -1,23 +1,33 @@
-function [session, init_params, glm_params] = fLocGearRun(session, config)
-% Generate data structures of vistasoft parameters for preprocessing and 
-% analyzing fLoc data with a GLM using a Flywheel gear. 
+function err = fLocGearRun(session, config)
+% Generate data structures of vistasoft parameters for preprocessing and
+% analyzing fLoc data with a GLM using a Flywheel gear.
 %
 % INPUTS
 % 1) session -- path to data directory in Flywheel (char array)
 % 2) config -- path to Flywheel config file (char array)
-% 
+%
 % OUPUTS
 % 1) session -- path to data directory in Flywheel (char array)
 % 2) init_params -- parameters for initialization/preprocessing (struct)
 % 3) glm_params -- parameters for running GLM analysis (struct)
-% 
+%
 % AS 8/2018
 
+%% Parse config file for params.
+
+% Read the json file
+config = jsonread(config);
+
+% Set the params
+clip = config.config.clip;
+
+% Currently we're not setting those params in the config/manifest
+% file, thus we set the defaults below.
+[~, init_params, glm_params] = fLocAnalysisParams(session, clip);
 
 %% Run fLoc Analysis
 err = fLocAnalysis(session, init_params, glm_params, clip, QA);
 
 clear global
-
 
 end

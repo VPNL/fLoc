6c148300c349a8587432c6b61660a001e866313f
Michael Perry <lmperry@stanford.edu>
2018-11-09 15:16:55 -0800

    Improve build script

--- gear/bin/fLocGearRun_Build.m
+++ gear/bin/fLocGearRun_Build.m
@@ -8,27 +8,30 @@ if (strfind(version, '9.3.0') ~= 1) || (strfind(computer, 'GLNXA64') ~= 1)
     error('You must compile this function using R2017b (9.3.0.713579) 64-bit (glnxa64). You are using %s, %s', version, computer);
 end
 
+disp(mfilename('fullpath'));
+compileDir = fileparts(mfilename('fullpath'));
+if ~strcmpi(pwd, compileDir)
+    disp('You must run this code from %s', compileDir);
+end
+
+
 % Download the source code
 disp('Cloning source code...');
-system('git clone -b gear https://github.com/vpnl/fLoc');
 system('git clone https://github.com/vistalab/vistasoft');
 
 % Set paths
 disp('Adding paths to build scope...');
 restoredefaultpath;
-addpath(genpath(fullfile(pwd, 'fLoc')));
-rmpath(genpath(fullfile(pwd, 'fLoc', '.git')));
 addpath(genpath(fullfile(pwd, 'vistasoft')));
-rmpath(genpath(fullfile(pwd, 'vistasoft', '.git')));
+addpath(genpath('../../fLoc'))
 
 % Compile
 disp('Running compile code...');
-mcc -m fLoc/functions/fLocGearRun.m
+mcc -v -R -nodisplay -m ../../functions/fLocGearRun.m
 
 % Clean up
 disp('Cleaning up...')
-rmdir(fullfile(pwd, 'fLoc'), 's')
-rmdir(fullfile(pwd, 'vistasoft'), 's')
+rmdir(fullfile(pwd, 'vistasoft'), 's');
 
 disp('Done!');
 exit
